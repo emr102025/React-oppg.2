f2e30ead0292b174b771e29c67218de2cd6d285a
emr102025 <emr102025@gmail.com>
2026-02-03 20:04:15 +0100

    Made styles folder and imported to Cookie file

--- React_oppg.2/src/components/CookieClicker.jsx
+++ React_oppg.2/src/components/CookieClicker.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
+import styles from "../styles/CookieClicker.module.css";
 import cookie from "/cookie.png";
 
 export default function CookieClicker() {
@@ -7,7 +8,11 @@ export default function CookieClicker() {
     <>
       <h1>CookieClicker</h1>
       <button>
-        <img src={cookie} alt="An animated happy cookie with arms and legs" />
+        <img
+          src={cookie}
+          alt="An animated happy cookie with arms and legs"
+          className={styles.cookie_img}
+        />
       </button>
     </>
   );
