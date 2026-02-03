10d7447a92d4c69ec1b827df2d5898e8f99cc61a
emr102025 <emr102025@gmail.com>
2026-02-03 19:56:20 +0100

    Imported CookieClicker to app and added button with img

--- React_oppg.2/src/components/CookieClicker.jsx
+++ React_oppg.2/src/components/CookieClicker.jsx
@@ -1,7 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import cookie from "/cookie.png";
+
 export default function CookieClicker() {
   return (
     <>
       <h1>CookieClicker</h1>
+      <button>
+        <img src={cookie} alt="An animated happy cookie with arms and legs" />
+      </button>
     </>
   );
 }
