b4eb7eac331474d9872afdb11a37de04a5868a72
emr102025 <emr102025@gmail.com>
2026-02-04 12:13:52 +0100

    Used useState og useEffect, and implemented in a function

--- React_oppg.2/src/components/CatFacts.jsx
+++ React_oppg.2/src/components/CatFacts.jsx
@@ -3,6 +3,28 @@ import { useState, useEffect } from "react";
 import styles from "../styles/CatFacts.module.css";
 
 export default function CatFacts() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [catFacts, setcatFacts] = [];
+
+  useEffect(() => {
+    const fetchCatFacts = async () => {
+      try {
+        const response = await fetch("https://catfact.ninja/facts?limit=5");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setcatFacts(data.data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCatFacts();
+  });
+
   return (
     <>
       <h1 className={styles.h1}>CatFacts</h1>
