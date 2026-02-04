a1802493aef2ef33a43d1bcf253930c1ede3ac15
emr102025 <emr102025@gmail.com>
2026-02-04 10:49:41 +0100

    CatFacts file with css made and imported where they need access

--- React_oppg.2/src/App.jsx
+++ React_oppg.2/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import CatFacts from "./components/CatFacts";
 import CookieClicker from "./components/CookieClicker";
 
 function App() {
   return (
     <>
       <CookieClicker />
+      <CatFacts />
     </>
   );
 }
---
+++ React_oppg.2/src/components/CatFacts.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react";
+import styles from "../styles/CatFacts.module.css";
+
+export default function CatFacts() {
+  return (
+    <>
+      <h1 className={styles.h1}>CatFacts</h1>
+    </>
+  );
+}
