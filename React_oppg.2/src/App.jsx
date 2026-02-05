4ddd9fe259a5256a4a7098d62da698de089af8b0
emr102025 <emr102025@gmail.com>
2026-02-05 11:10:30 +0100

    Made users.jsx file an imported it to app.jsx file

--- React_oppg.2/src/App.jsx
+++ React_oppg.2/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import CatFacts from "./components/CatFacts";
 import CookieClicker from "./components/CookieClicker";
+import Users from "./components/Users";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="flexbox">
         <CookieClicker />
         <CatFacts />
+        <Users />
       </div>
     </>
   );
