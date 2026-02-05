03eb48e873965acb74552bfeb9c3ebb67fae3101
emr102025 <emr102025@gmail.com>
2026-02-05 14:18:02 +0100

    Added the mockData to the page

--- React_oppg.2/src/components/Users.jsx
+++ React_oppg.2/src/components/Users.jsx
@@ -1,7 +1,26 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { mockData } from "../data/mockdata";
+
 export default function Users() {
+  const Users = () => {
+    const [user, setUser] = useState(mockData);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+  };
+
   return (
     <>
       <h1>Users</h1>
+      <ul>
+        {mockData.map((user) => (
+          <li>
+            Username: {user.username}
+            <br />
+            Email: {user.email}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
